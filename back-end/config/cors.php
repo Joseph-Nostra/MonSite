478107bfdc3e381e8faacdc9d90813c912319a2c
Ton Nom <ton.email@example.com>
2026-06-24 01:48:58 +0100

    Fix: resolve registration CORS error and disable Reverb service as requested

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -19,16 +19,19 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173'
+    ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
